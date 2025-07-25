fdc75e8f77ba60fff2ca5bd038bd4b20c8fed634
yenkj <49618620+yenkj@users.noreply.github.com>
2025-07-25 11:07:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,80 +1,134 @@
+// server.js
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const PORT = process.env.PORT || 3000;
-
 app.use(express.static('public'));
 
-const rooms = {};
+const rooms = {}; // 保存每个房间的信息
 
-io.on('connection', socket => {
-  let currentRoom = null;
-  let isHost = false;
+io.on('connection', (socket) => {
+  console.log('用户连接:', socket.id);
 
-  socket.on('joinRoom', ({roomId, host}) => {
-    currentRoom = roomId;
-    isHost = host;
-    socket.join(roomId);
+  socket.on('joinRoom', ({ room, password, username }, callback) => {
+    if (!room || !password || !username) {
+      return callback({ success: false, message: '参数不完整' });
+    }
 
-    if (!rooms[roomId]) {
-      rooms[roomId] = {
-        hostId: isHost ? socket.id : null,
-        users: new Set(),
-        playState: { paused: true, time: 0 }
+    if (!rooms[room]) {
+      rooms[room] = {
+        password,
+        hostId: null,
+        videoUrl: '',
+        subtitleUrl: '',
+        videoState: {
+          playing: false,
+          currentTime: 0,
+          lastUpdate: Date.now(),
+        },
+        users: {},
       };
+    } else if (rooms[room].password !== password) {
+      return callback({ success: false, message: '密码错误' });
     }
-    rooms[roomId].users.add(socket.id);
 
-    // 如果是主持人，设定hostId
-    if (isHost) rooms[roomId].hostId = socket.id;
+    socket.join(room);
+    rooms[room].users[socket.id] = username;
+    socket.data.room = room;
+    socket.data.username = username;
 
-    // 给新加入的客户端同步当前播放状态
-    socket.emit('syncState', rooms[roomId].playState);
+    // 初始状态发送
+    socket.emit('videoUpdate', {
+      videoUrl: rooms[room].videoUrl,
+      subtitleUrl: rooms[room].subtitleUrl,
+    });
+    socket.emit('videoState', rooms[room].videoState);
 
-    // 广播当前在线人数
-    io.to(roomId).emit('userCount', rooms[roomId].users.size);
+    // 广播新用户加入
+    socket.to(room).emit('chatMessage', {
+      id: '系统',
+      msg: `${username} 加入了房间`
+    });
 
-    // 通知聊天室
-    io.to(roomId).emit('chatMessage', {user: '系统', message: `用户${socket.id.substring(0,4)}加入房间`});
+    return callback({ success: true });
   });
 
-  socket.on('playbackAction', ({roomId, action, currentTime}) => {
-    if (!rooms[roomId]) return;
-    // 只有主持人能控制
-    if (socket.id !== rooms[roomId].hostId) {
-      socket.emit('errorMessage', '只有主持人可控制播放');
-      return;
+  socket.on('loginAsHost', ({ password }, callback) => {
+    const room = socket.data.room;
+    if (!room || !rooms[room]) return;
+    if (rooms[room].hostId) return callback(false);
+    if (password === 'admin123') {
+      rooms[room].hostId = socket.id;
+      socket.emit('hostAssigned');
+      callback(true);
+    } else {
+      callback(false);
     }
-    // 更新播放状态
-    if (action === 'play') rooms[roomId].playState.paused = false;
-    else if (action === 'pause') rooms[roomId].playState.paused = true;
-    else if (action === 'seek') rooms[roomId].playState.time = currentTime;
-
-    rooms[roomId].playState.time = currentTime;
-
-    // 同步给其他观众
-    socket.to(roomId).emit('syncAction', {action, currentTime});
   });
 
-  socket.on('sendMessage', ({roomId, user, message}) => {
-    io.to(roomId).emit('chatMessage', {user, message});
+  socket.on('chatMessage', (msg) => {
+    const room = socket.data.room;
+    if (!room || !rooms[room]) return;
+    io.to(room).emit('chatMessage', {
+      id: socket.data.username || socket.id,
+      msg,
+    });
   });
 
-  socket.on('disconnecting', () => {
-    if (!currentRoom) return;
-    if (rooms[currentRoom]) {
-      rooms[currentRoom].users.delete(socket.id);
-      if (rooms[currentRoom].hostId === socket.id) {
-        rooms[currentRoom].hostId = null;
-        io.to(currentRoom).emit('chatMessage', {user:'系统', message:'主持人已离开，播放控制失效'});
-      }
-      io.to(currentRoom).emit('userCount', rooms[currentRoom].users.size);
+  socket.on('hostAddVideo', (url) => {
+    const room = socket.data.room;
+    if (!room || !rooms[room]) return;
+    if (rooms[room].hostId !== socket.id) return;
+    rooms[room].videoUrl = url;
+    io.to(room).emit('videoUpdate', {
+      videoUrl: url,
+      subtitleUrl: rooms[room].subtitleUrl,
+    });
+  });
+
+  socket.on('hostAddSubtitle', (url) => {
+    const room = socket.data.room;
+    if (!room || !rooms[room]) return;
+    if (rooms[room].hostId !== socket.id) return;
+    rooms[room].subtitleUrl = url;
+    io.to(room).emit('videoUpdate', {
+      videoUrl: rooms[room].videoUrl,
+      subtitleUrl: url,
+    });
+  });
+
+  socket.on('hostControl', (state) => {
+    const room = socket.data.room;
+    if (!room || !rooms[room]) return;
+    if (rooms[room].hostId !== socket.id) return;
+    rooms[room].videoState = { ...state, lastUpdate: Date.now() };
+    socket.to(room).emit('videoState', rooms[room].videoState);
+  });
+
+  socket.on('disconnect', () => {
+    const room = socket.data.room;
+    if (!room || !rooms[room]) return;
+
+    const username = socket.data.username;
+    delete rooms[room].users[socket.id];
+
+    if (rooms[room].hostId === socket.id) {
+      rooms[room].hostId = null;
+      io.to(room).emit('chatMessage', { id: '系统', msg: `主持人 ${username} 离开了房间` });
+    } else {
+      io.to(room).emit('chatMessage', { id: '系统', msg: `${username} 离开了房间` });
+    }
+
+    // 如果房间空了，就删除
+    if (Object.keys(rooms[room].users).length === 0) {
+      delete rooms[room];
+      console.log('房间被销毁:', room);
     }
   });
 });
 
+const PORT = 3000;
 http.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
